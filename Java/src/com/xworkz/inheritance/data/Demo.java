ccf9a40307489101bbecca589a4a6729a33b2333
K Shivakumar <shivakumar.k.xworkz@gmail.com>
2023-02-14 09:21:06 +0530

    gfjgh

---
+++ Java/src/com/xworkz/inheritance/data/Demo.java
@@ -0,0 +1,11 @@
+package com.xworkz.inheritance.data;
+
+public class Demo {
+
+	public static void main(String[] args) 
+	{
+	System.out.println("HI World");	// TODO Auto-generated method stub
+
+	}
+
+}
